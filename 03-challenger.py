f8cb3a56e1fadcf3344272a36db260ea08795e0c
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-05-15 17:23:07 +0100

    calculate la Surface/Peremettre du rectangle

---
+++ 03-challenger.py
@@ -0,0 +1,8 @@
+largeur = float(input("Saisir la largeur du Rectangle : "))
+longeur = float(input("Saisir la longeur du Rectangle : "))
+
+Surface = largeur * longeur
+Peremettre = (largeur + longeur) * 2
+
+print("la Surface du Rectangle est de :", Surface)
+print("le Peremettre du Rectangle est de :", Peremettre)
---
+++ 04-challenger.py
@@ -0,0 +1,6 @@
+X = input("Saisir la valuer the X: ")
+Y = input("Saisir la valeur de Y: ")
+
+Puissance = X ** Y
+
+print("la Puissance est de :" , Puissance)
