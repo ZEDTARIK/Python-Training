84b46e51448f1ab5e4080198e91d55d4be75c8ce
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-05-17 19:39:54 +0100

    calculer le volue de sphere

---
+++ 06-challenger.py
@@ -0,0 +1,5 @@
+import math
+Rayon = float(input("Saisir la valeur de sphere"))
+Volume = (4 * math.pi * (Rayon**3)) /3 
+
+print("le volume de sphere est de : ", format(Volume, ".2f"))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-birthYear = int(input('Please write your birth year : '))
-currentAge = 2023 - birthYear
+import math
+Rayon = float(input("Saisir la valeur de sphere"))
+Volume = (4 * math.pi * (Rayon**3)) /3 
 
-print("your are the ", currentAge, " years old")
+print("le volume de sphere est de : ", format(Volume, ".2f"))
